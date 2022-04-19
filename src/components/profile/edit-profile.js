810283ab114a1279a15285b69ce4ac9b91ca8089
animeshm1995 <animesh.mukul@gmail.com>
2022-04-19 15:12:22 -0400

    update user changes

--- src/components/profile/edit-profile.js
+++ src/components/profile/edit-profile.js
@@ -1,46 +1,27 @@
-import React, {useEffect} from "react";
-import {useState} from "react";
-import {useNavigate,Link} from "react-router-dom";
-import * as service from "../../services/users-service";
-import {updateUser} from "../../services/users-service";
+import {Link} from "react-router-dom";
 
 const EditProfile = () => {
-    const [newUser,setNewUser] = useState({});
-    const navigate = useNavigate();
-    const findMyProfile = () =>
-        service.findUserById("my")
-            .then(newUser => setNewUser(newUser));
-    useEffect(findMyProfile, []);
-    const editProfile = () =>
-        service.updateUser("my",newUser)
-            .then((user) => navigate('/profile/mytuits'))
-            .catch(e => alert(e));
-    const deleteProfile = () =>
-        service.deleteUser("my")
-            .then((user) => navigate('/profile/signup'))
-            .catch(e => alert(e));
     return(
         <div className="ttr-edit-profile">
             <div className="border border-bottom-0">
                 <Link to="/profile" className="btn btn-light rounded-pill fa-pull-left fw-bolder mt-2 mb-2 ms-2">
                     <i className="fa fa-close"></i>
                 </Link>
-                <Link to="/profile" className="btn btn-dark rounded-pill fa-pull-right fw-bolder mt-2 mb-2 me-2"
-                      onClick = {editProfile}>
+                <Link to="/profile" className="btn btn-dark rounded-pill fa-pull-right fw-bolder mt-2 mb-2 me-2">
                     Save
                 </Link>
                 <h4 className="p-2 mb-0 pb-0 fw-bolder">Edit profile</h4>
                 <div className="mb-5 position-relative">
-                    <img className="w-100" src="../../../public/images/nasa-profile-header.jpg"/>
+                    <img className="w-100" src="../images/nasa-profile-header.jpg"/>
                     <div className="bottom-0 left-0 position-absolute">
                         <div className="position-relative">
                             <img className="position-relative ttr-z-index-1 ttr-top-40px ttr-width-150px"
-                                 src="../../../public/images/nasa-3.png"/>
+                                 src="../images/nasa-3.png"/>
                         </div>
                     </div>
                 </div>
             </div>
-            <form action="../../../public/html/profile.html">
+            <form action="profile.html">
                 <div className="border border-secondary rounded-3 p-2 mb-3">
                     <label htmlFor="username">Username</label>
                     <input id="username" title="Username" readOnly
@@ -51,18 +32,13 @@ const EditProfile = () => {
                     <label htmlFor="first-name">First name</label>
                     <input id="first-name"
                            className="p-0 form-control border-0"
-                           onChange={(e) =>
-                               setNewUser({...newUser, firstName: e.target.value})}
-                           placeholder="first-name" />
-
+                           placeholder="Alan"/>
                 </div>
                 <div className="border border-secondary rounded-3 p-2 mb-3">
                     <label htmlFor="last-name">Last name</label>
                     <input id="last-name"
                            className="p-0 form-control border-0"
-                           onChange={(e) =>
-                               setNewUser({...newUser, lastName: e.target.value})}
-                           placeholder="last-name"/>
+                           placeholder="Turin"/>
                 </div>
                 <div className="border border-secondary rounded-3 p-2 mb-3">
                     <label htmlFor="bio">Bio</label>
@@ -74,74 +50,63 @@ const EditProfile = () => {
                     <label htmlFor="date-of-birth">Date of birth</label>
                     <input id="date-of-birth"
                            className="p-0 form-control border-0"
-                           onChange={(e) =>
-                               setNewUser({...newUser, dateOfBirth: e.target.value})}
-                           placeholder="date-of-birth"/>
+                           type="date" value="2003-01-02"/>
                 </div>
                 <div className="border border-secondary rounded-3 p-2 mb-3">
                     <label htmlFor="email">Email</label>
-                    <input id="email"
+                    <input id="email" placeholder="alan@cam.ac.uk"
                            className="p-0 form-control border-0"
-                           onChange={(e) =>
-                               setNewUser({...newUser, email: e.target.value})}
-                           type="email" placeholder="email"/>
-                </div>{/*
-            <div className="border border-secondary rounded-3 p-2 mb-3">
-              <label htmlFor="password">Reset password</label>
-              <input id="password"
-                     className="p-0 form-control border-0"
-                     type="password"/>
-            </div>*/}
+                           type="email"/>
+                </div>
+                <div className="border border-secondary rounded-3 p-2 mb-3">
+                    <label htmlFor="password">Reset password</label>
+                    <input id="password"
+                           className="p-0 form-control border-0"
+                           type="password"/>
+                </div>
                 <div className="border border-secondary rounded-3 p-2 mb-3">
                     <label for="photo">Profile photo</label>
                     <input id="photo"
                            className="p-0 form-control border-0"
-                           onChange={(e) =>
-                               setNewUser({...newUser, profilePhoto: e.target.value})}
-                           type="file" placeholder="photo"/>
+                           type="file"/>
                 </div>
                 <div className="border border-secondary rounded-3 p-2 mb-3">
                     <label for="header">Header image</label>
                     <input id="header"
                            className="p-0 form-control border-0"
-                           onChange={(e) =>
-                               setNewUser({...newUser, headerImage: e.target.value})}
-                           type="file" placeholder="photo"/>
+                           type="file"/>
                 </div>
                 <div className="border border-secondary rounded-3 p-2 mb-3">
                     <label for="account">Select account</label>
                     <select
                         className="p-0 form-control border-0"
                         id="account">
-                        <option>PERSONAL</option>
-                        <option>ACADEMIC</option>
-                        <option>PROFESSIONAL</option>
-                        onChange={(e) =>
-                        setNewUser({...newUser, accountType: e.target.value})}
+                        <option>Personal account</option>
+                        <option selected>Academic account</option>
                     </select>
                 </div>
                 <div className="border border-secondary rounded-3 p-2 mb-3">
                     Marital status
-                    <input id="MARRIED"
-                           type="radio" name="maritalStatus"/>
-                    <label for="MARRIED">Married</label>
-                    <input id="SINGLE" type="radio"
-                           checked name="maritalStatus"/>
-                    <label for="SINGLE">Single</label>
-                    <input id="WIDOWED" type="radio"
-                           checked name="maritalStatus"/>
-                    <label for="WIDOWED">WIDOWED</label>
-                    onChange={(e) =>
-                    setNewUser({...newUser, maritalStatus: e.target.value})}
+                    <input id="married"
+                           type="radio" name="marital"/>
+                    <label for="married">Married</label>
+                    <input id="single" type="radio"
+                           checked name="marital"/>
+                    <label for="single">Single</label>
                 </div>
-                <button onClick={editProfile}
-                        className="btn btn-primary mb-5">Save
-                </button>
-                <button onClick={deleteProfile}
-                        className="btn btn-primary mb-5">Delete Profile
-                </button>
-            </form>
-        </div>
+                <div className="border border-secondary rounded-3 p-2 mb-3">
+                    Topics of interest
+                    <input id="space" type="checkbox"
+                           checked name="topics"/>
+                    <label for="space">Space</label>
+                    <input id="energy" type="checkbox" checked
+                           name="topics"/>
+                    <label for="energy">Energy</label>
+                    <input id="politics" type="checkbox"
+                           name="topics"/>
+                    <label for="politics">Politics</label>
+                </div>
+            </form></div>
     );
 };
 export default EditProfile;
