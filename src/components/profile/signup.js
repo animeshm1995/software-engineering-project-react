38dc2df9fa63871ac34db09c6af88451151f76cf
LAPTOP-U8I5ON0L\shrey <shreyajain11197@gmail.com>
2022-04-24 16:06:33 -0400

    Update Profile changes

--- src/components/profile/signup.js
+++ src/components/profile/signup.js
@@ -5,10 +5,12 @@ import {useNavigate} from "react-router-dom";
 const Signup = () => {
     const [newUser, setNewUser] = useState({});
     const navigate = useNavigate();
-    const signup = () =>
+    const signup = () => {
+        newUser.headerImage = "../../images/perseverance.jpg";
         service.register(newUser)
             .then(() => navigate('/home'))
             .catch(e => alert(e));
+    }
     return (
         <div>
             <h1>Signup</h1>
