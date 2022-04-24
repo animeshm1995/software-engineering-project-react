41cf13395de6a47ecf065134ff9d346c82890a16
animeshm1995 <animesh.mukul@gmail.com>
2022-04-24 12:33:34 -0400

    file upload changes

--- src/components/profile/edit-profile.js
+++ src/components/profile/edit-profile.js
@@ -42,7 +42,7 @@ const EditProfile = () => {
                     <div className="bottom-0 left-0 position-absolute">
                         <div className="position-relative">
                             <img className="position-relative ttr-z-index-1 ttr-top-40px ttr-width-150px"
-                                 src="../../../public/images/nasa-3.png"/>
+                                 src={newUser.profilePhoto}/>
                         </div>
                     </div>
                 </div>
